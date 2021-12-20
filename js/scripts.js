8d24442d7d21855c7ad9b2a21a17ad82d97db195
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2021-12-20 13:32:07 -0800

    added search feature

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,8 @@ const closeBtn = document.querySelector('.close');
 const nextBtn = document.querySelector('.next');
 const prevBtn = document.querySelector('.prev');
 const search = document.querySelector('#search');
+const searchInput = document.querySelector('input'); 
+const desc = document.querySelector('.desc'); 
 
 /*==============
  EVENT LISTENERS
@@ -17,16 +19,31 @@ const search = document.querySelector('#search');
 //OPENS AND DISPLAYS MODAL WINDOW WHEN ONE OF THE IMAGES IS CLICKED
 container.addEventListener('click', function(event) {
   if(event.target.classList.contains('img')){
-    modal.classList.add('open');
+    modal.style.display = 'block'; 
     modalImg.src = event.target.src;
+    desc.textContent = event.target.dataset.title; 
   }
-})
+}); 
+
+//search functionality
+searchInput.addEventListener('keyup', () => {
+  const searchVal = searchInput.value; 
+  images.forEach((img) => {
+    if(!img.dataset.title.includes(searchVal)){
+      img.style.display = 'none'; 
+    }else {
+      img.style.display = 'block'; 
+    }
+  }); 
+  
+  if(!searchVal) images.forEach((img) => img.style.display = 'block'); 
+}); 
 
 // // WHEN THE MODAL WINDOW IS OPEN IT WILL CHANGE IMAGES OR CLOSE THE MODAL WINDOW DEPENDING ON ICONS CLICKED
 modal.addEventListener('click', function(event){
   //CLOSES THE MODAL WINDOW
   if(event.target.classList.contains('close')){
-    modal.classList.remove('open');
+    modal.style.display = 'none'; 
   }
   //DISPLAYS A DIFFERENT IMAGE WHEN next & previous IMAGES ARE CLICKED
   if(event.target.classList.contains('prev')){
@@ -37,6 +54,7 @@ modal.addEventListener('click', function(event){
     }else{
       modalImg.src = images[images.length-1].src;
     }
+    desc.textContent = currentImg.dataset.title;
   }
 
   if(event.target.classList.contains('next')){
@@ -47,5 +65,6 @@ modal.addEventListener('click', function(event){
     }else{
       modalImg.src = images[0].src; 
     }
+    desc.textContent = currentImg.dataset.title;
   }
-})
+}); 
